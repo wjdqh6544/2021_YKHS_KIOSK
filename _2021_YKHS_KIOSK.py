a70b8ed0f472fe3a4842af268eb4f7ed4b9857d0
wjdqh6544 <wjdqh6544@naver.com>
2021-08-16 00:27:02 +0900

    2021.08.16

--- _2021_YKHS_KIOSK.py
+++ _2021_YKHS_KIOSK.py
@@ -1,1 +1,10 @@
+from tkinter import *
+
+root=Tk()
+root.title("YKHS Cafeteria KIOSK v1.0 (Alpha)")
+root.attributes('-fullscreen', True) # 추후 Alt+F4 방지하기. 종료는 관리자 로그인 필요.
+
+btn1=Button(root, padx=10. pady=10, width=100, height=100, text="선결재")
+btn1.pack()
+root.mainloop()
 
