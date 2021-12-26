6e31cafb8c14657fb74736b1f8274b929c6492e2
형철 서 <wjdqh6544@naver.com>
2021-12-26 16:28:57 +0900

    2021.12.26

--- 2021_YKHS_KIOSK.py
+++ 2021_YKHS_KIOSK.py
@@ -1,34 +1,26 @@
 from tkinter import *
-import time
-import threading
+from tkinter import messagebox as msg
 
 root=Tk()
 root.title("YKHS Cafeteria KIOSK v1.0 (Alpha)")
 root.attributes('-fullscreen', True) # 추후 Alt+F4, 작업관리자 등 방지하기. 종료는 관리자 로그인 필요.
 
+def 준비중입니다():
+    msg.showinfo("준비중입니다.","준비중입니다."
+                 )
 def main():
-    def t2label():
-        showtime=time.strftime("%H:%M:%S")
-        t2=Label(frame, font=('맑은 고딕', 30, 'bold'), fg='blue', text="\n현재 시각: "+str(showtime))
-        t2.pack()
-        time.sleep(1)
-        t2.forget()
-        threading.Timer(0, t2label).start()
-    
     exit=Button(root, font=("맑은 고딕", 15),  text="EXIT", command=root.destroy)
     exit.pack(anchor=NE)
 
-    frame=Frame(root, bd=40, bg='gray')
-    frame.pack(anchor=CENTER)
-
-    t1=Label(frame, font=('맑은 고딕', 45, 'bold'), text="\n영광고등학교 매점 키오스크\n")
+    t1=Label(root, font=('맑은 고딕', 45, 'bold'), text="\n영광고등학교 매점 키오스크\n")
     t1.pack()
-    t2label()
     
-    btn1=Button(root, padx=10, pady=10, width=25, height=10, text="예치금 충전하기")
-    btn1.pack(side=LEFT)
-    btn1=Button(root, padx=10, pady=10, width=25, height=10, text="주문하기")
-    btn1.pack(side=RIGHT)
+
+
+    btn1=Button(root, font=('맑은 고딕', 30), padx=30, pady=10, width=20, height=3, text="예치금 충전하기")
+    btn1.pack(side=LEFT, padx=250)
+    btn1=Button(root, font=('맑은 고딕', 30), padx=30, pady=10, width=20, height=3, text="주문하기", command=준비중입니다)
+    btn1.pack(side=RIGHT, padx=250)
     root.mainloop()
 
 main()
