49eca12a6ea65707b639b13b5cc4168d9fd7909f
형철 서 <wjdqh6544@naver.com>
2021-12-27 14:52:19 +0900

    2021.12.27-main디자인완료

--- 2021_YKHS_KIOSK.py
+++ 2021_YKHS_KIOSK.py
@@ -19,33 +19,39 @@ def 준비중입니다():  #Push 팝업
     msg.showinfo("준비중입니다.","준비중입니다.")
 
 def main():
-    #blank_0=Label(root, text="", width=100)
-    #blank_0.grid(row=0, column=0)
+    # Title ~ Login Form 여백
+    blank_r2c0=Label(root, text="", height=8, width=50) 
+    blank_r2c0.grid(row=2, column=0)
+    blank_r5c0=Label(root, text="", width=50, height=12)
+    blank_r5c0.grid(row=5, column=0)
+
+
     exit=Button(root, font=("맑은 고딕", 15),  text="EXIT", command=root.destroy)
-    exit.grid(row=0, column=0, sticky="W")  #exit.place(x=20, y=30, anchor="ne")
+    exit.grid(row=0, column=0, sticky="W")
     title=Label(root, font=('맑은 고딕', 45, 'bold'), text="\n영광고등학교 매점 키오스크\n") 
-    title.grid(row=1, column=2, sticky="")
-    blank_row2=Label(root, text="", width=1)
-    blank_row2.grid(row=2, column=0, columnspan=5)
+    title.grid(row=1, column=1, sticky="", columnspan=5)
+
+
  
     username=StringVar()
     password=StringVar()## ID, PW 변수로 저장
     usernameLabel=Label(root, text="UserName(ID)", font=('맑은 고딕', 30, 'bold'))
-    usernameEntry=Entry(root, textvariable=username)
+    usernameEntry=Entry(root, textvariable=username, font=('맑은 고딕', 20))
     usernameLabel.grid(row=3, column=1, sticky="E")
     usernameEntry.grid(row=3, column=2, sticky="W", padx=100, ipadx=100, ipady=15)
     passwdLabel=Label(root, text="Password", font=('맑은 고딕', 30, 'bold'))
-    passwdEntry=Entry(root, textvariable=password)
+    passwdEntry=Entry(root, textvariable=password, font=('맑은 고딕', 20), show="*")
     passwdLabel.grid(row=4, column=1, sticky="E")
     passwdEntry.grid(row=4, column=2, sticky="W", padx=100, pady=15, ipadx=100, ipady=15)
-    LoginBtn=Button(root, font=("맑은 고딕", 30), padx=10, pady=10, width=10, height=1, text="Login", command=준비중입니다)
-    LoginBtn.grid(row=3, column=4, rowspan=2)
-    blank_row5=Label(root, text="", width=1)
-    blank_row5.grid(row=5, column=0, columnspan=5)
+    LoginBtn=Button(root, font=("맑은 고딕", 30), padx=10, pady=10, width=10, text="계정 로그인", command=준비중입니다)
+    LoginBtn.grid(row=3, column=3, rowspan=2)
 
-    btn1=Button(root, font=('맑은 고딕', 30), padx=30, pady=10, width=20, height=3, text="예치금 충전하기", command=준비중입니다)
-    btn1.grid(row=6, column=1)
-    btn1=Button(root, font=('맑은 고딕', 30), padx=30, pady=10, width=20, height=3, text="주문하기", command=준비중입니다)
+
+    RegisBtn=Button(root, font=("맑은 고딕", 30), padx=10, pady=10, width=10, text="계정 만들기", command=준비중입니다)
+    RegisBtn.grid(row=6, column=1)
+    btn1=Button(root, font=('맑은 고딕', 30), padx=30, pady=10, text="예치금 충전하기", command=준비중입니다)
     btn1.grid(row=6, column=2)
+    btn1=Button(root, font=('맑은 고딕', 30), padx=30, pady=10, text="주문하기", command=준비중입니다)
+    btn1.grid(row=6, column=3)
     root.mainloop()
 main()
